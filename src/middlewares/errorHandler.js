5f8e91b60ce1a36e82ad028f31ca65036991a01e
Matias Zabala <zabala@live.com.ar>
2025-03-18 17:10:42 -0300

    Segunda entrega

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,7 +3,6 @@ const errorhandler=(error, req , res , next)=>{
     console.log(`Error: ${error.message}`)
     res.setHeader('Content-Type', 'application/json')
     return res.status(500).json(
-
         {
             error: `Error inesperado en el servidor - Intente mas tarde o contacte a su proovedor`,
             detalle: `${error.message}`
